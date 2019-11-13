5dbcd28ddb0e758f49571717ab2638ca76ca5d88
Filip <filipmymail@gmail.com>
2019-11-13 19:04:27 +0100

    Create zadatak8.js
    Znaci ni zadnji pull request video ali... koju nedelju unazad sam izgubio 0.5 poena jer nisam umeo ovo da dodam pipe funkciji... a ni ti nisi imao lmao

---
+++ lab_3/zadatak8.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var file = fs.createReadStream('origin.txt');
+var destFile = fs.createWriteStream('destination.txt');
+// TO DO
+file.pipe(destFile,{end:false});
+file.on('end', function(){
+	destFile.end('Finished!');
+});
